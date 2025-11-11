478c71f031de2d1474360bbe2197bf18dfafef17
Navid M <navidmomtahen@gmail.com>
2025-11-11 18:36:48 +0000

    amend

--- src/axe.nim
+++ src/axe.nim
@@ -11,7 +11,7 @@ else:
 
 when isMainModule:
     if paramCount() < 1:
-        echo "usage: axe input.axe [-e = emit generated code]"
+        echo "usage: axe input.axe\n\t[-e = emit generated code as file | -asm = emit assembly code]"
     else:
         try:
             var name = paramStr(1)
@@ -21,21 +21,37 @@ when isMainModule:
                 source = readFile(name)
                 tokens = lex(source)
                 ast = parse(tokens)
-                cCode = generateC(ast)
-
-            writeFile(name.replace(".axe", ".c"), cCode)
-            discard execProcess(
-                command = "gcc",
-                args = [name.replace(".axe", ".c"), "-o", name.replace(".axe", extension)],
-                options = {poStdErrToStdOut}
-            )
-
-            if "-e" notin commandLineParams():
-                removeFile(name.replace(".axe", ".c"))
+                
+            if "-asm" in commandLineParams():
+                let asmCode = generateAsm(ast)
+                writeFile(name.replace(".axe", ".asm"), asmCode)
+                discard execProcess(
+                    command = "nasm",
+                    args = ["-f", "elf32", name.replace(".axe", ".asm"), "-o", name.replace(".axe", ".o")],
+                    options = {poStdErrToStdOut}
+                )
+                discard execProcess(
+                    command = "gcc",
+                    args = ["-m32", name.replace(".axe", ".o"), "-o", name.replace(".axe", extension)],
+                    options = {poStdErrToStdOut}
+                )
+                if "-e" notin commandLineParams():
+                    removeFile(name.replace(".axe", ".asm"))
+                    removeFile(name.replace(".axe", ".o"))
+            else:
+                let cCode = generateC(ast)
+                writeFile(name.replace(".axe", ".c"), cCode)
+                discard execProcess(
+                    command = "gcc",
+                    args = [name.replace(".axe", ".c"), "-o", name.replace(".axe", extension)],
+                    options = {poStdErrToStdOut}
+                )
+                if "-e" notin commandLineParams():
+                    removeFile(name.replace(".axe", ".c"))
 
         except ValueError as e:
             echo "Compilation error: ", e.msg
         except OSError as e:
-            echo "Linker error (maybe no C toolchain is installed?): ", e.msg.replace("OS error:", "")
+            echo "Linker error (maybe no C/NASM toolchain is installed?): ", e.msg.replace("OS error:", "")
         except Exception as e:
             echo "Error: ", e.msg
--- src/axe/renderer.nim
+++ src/axe/renderer.nim
@@ -24,3 +24,47 @@ proc generateC*(ast: ASTNode): string =
                 cCode.add("    }\n")
         cCode.add("    return 0;\n}\n")
     return cCode
+
+proc generateAsm*(ast: ASTNode): string =
+    ## Generate x86 assembly code from AST
+    ## Should be able to generate code for main function, loop and break statements, and string handling#
+    
+    var asmCode = ""
+    if ast.nodeType == "Main":
+        asmCode.add("""
+            section .data
+                fmt db "%s", 10, 0
+            section .text
+                global _start
+            _start:
+            """)
+        for child in ast.children:
+            case child.nodeType
+            of "Println":
+                asmCode.add(fmt"""
+                    push {child.value}
+                    push fmt
+                    call printf
+                    add esp, 8
+                """)
+            of "Loop":
+                asmCode.add("loop_start:\n")
+                for loopChild in child.children:
+                    case loopChild.nodeType
+                    of "Println":
+                        asmCode.add(fmt"""
+                            push {loopChild.value}
+                            push fmt
+                            call printf
+                            add esp, 8
+                        """)
+                    of "Break":
+                        asmCode.add("    jmp loop_end\n")
+                asmCode.add("    jmp loop_start\n")
+                asmCode.add("loop_end:\n")
+        asmCode.add("""
+            mov eax, 1
+            mov ebx, 0
+            int 0x80
+        """)
+    return asmCode
